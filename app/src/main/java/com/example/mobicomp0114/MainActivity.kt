dad3b716150bdd29704f4dce1cd3d5838c9e0128
youngj719 <youngj719@naver.com>
2020-01-22 08:26:52 +0900

    Second commit

--- app/src/main/java/com/example/mobicomp0114/MainActivity.kt
+++ app/src/main/java/com/example/mobicomp0114/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.mobicomp0114
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        test_button.setOnClickListener {
+            toast("Mobile Computing")
+        }
     }
 }
